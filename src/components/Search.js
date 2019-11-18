50828c4b70a4dbc0fc825d126f3663eb08920d24
Arman Sujoyan <asujoyan@gmail.com>
2019-11-18 16:29:57 +0400

    Extract search logic from SearchBar to Search component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { debounce } from 'lodash';
+
+import { getTournaments, clearTournaments } from '../redux/actions/tournamentsActions';
+import { addFavorite } from '../redux/actions/favoritesActions'
+import { tournamentSelector, tournamentLoadSelector } from '../redux/selectors/tournaments'
+
+import SearchDropdown from './SearchDropdown';
 import SearchBar from './SearchBar'
 
 export default function Search() {
+    const [showDropDown, setShowDropDown] = useState(false);
+    const [error, setError] = useState(false);
+    const [query, setQuery] = useState('');
+
+    const dispatch = useDispatch();
+    const foundTournaments = useSelector(tournamentSelector);
+    const isLoading = useSelector(tournamentLoadSelector);
+
+    const delayedQuery = useRef(debounce(
+        query => {
+        let error;
+        if (query.length > 0 && query.length < 2) {
+            error = true;
+        } else if(query.length !== 0) {
+            error = false;
+            dispatch(getTournaments(query));
+        } else {
+            dispatch(clearTournaments());
+        }
+        query.length >= 2 ? setShowDropDown(true) : setShowDropDown(false);
+        setError(error);
+        }, 400, { trailing: true, leading: false }
+    )).current;
+
+    const addFavoriteAction = id => {
+        dispatch(addFavorite(id));
+        setShowDropDown(false);
+        setQuery('');
+    };
+
+    const handleFocus = () => query.length >= 2 ? setShowDropDown(true) : null;
+
+    const handleChange = event => {
+        event.persist();
+        const { value } = event.target;
+        delayedQuery(value);
+        setQuery(value);
+    }
+
+    const handleDropdownItemClick = item => addFavoriteAction(item.id);
+
     return (
-        <div>
-            <SearchBar />
-        </div>
+        <>
+            <SearchBar
+                placeholder='Search tournaments...'
+                onChange={handleChange}
+                onFocus={handleFocus}
+                error={error}
+                value={query}/>
+            {
+                showDropDown ? <SearchDropdown
+                    listItems={foundTournaments}
+                    isLoading={isLoading}
+                    handleItemClick={handleDropdownItemClick}/> : null
+            }
+        </>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,6 @@
-import React, { useState, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { debounce } from 'lodash';
+import React from 'react';
 import { AppBar, Input, Toolbar } from '@material-ui/core'
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-
-import { getTournaments, clearTournaments } from '../redux/actions/tournamentsActions';
-import { addFavorite } from '../redux/actions/favoritesActions'
-import { tournamentSelector, tournamentLoadSelector } from '../redux/selectors/tournaments'
-import SearchDropdown from './SearchDropdown';
 
 const inputStyles = {
     root: {
@@ -30,69 +23,21 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-export default function SearchBar() {
-    const [query, setQuery] = useState('');
-    const [error, setError] = useState(false);
-    const [showDropDown, setShowDropDown] = useState(false);
-
-    const dispatch = useDispatch();
-    const foundTournaments = useSelector(tournamentSelector);
-    const isLoading = useSelector(tournamentLoadSelector);
-
+export default function SearchBar(props) {
     const classes = useStyles();
-    const delayedQuery = useRef(debounce(
-        query => {
-        let error;
-        if (query.length > 0 && query.length < 2) {
-            error = true;
-        } else if(query.length !== 0) {
-            error = false;
-            dispatch(getTournaments(query));
-        } else {
-            dispatch(clearTournaments())
-        }
-        query.length >= 2 ? setShowDropDown(true) : setShowDropDown(false);
-        setError(error);
-        }, 400, { trailing: true, leading: false }
-    )).current;
-
-    const handleChange = event => {
-        event.persist();
-        const { value } = event.target;
-        setQuery(value);
-        delayedQuery(value);
-    };
-
-    const addFavoriteAction = id => {
-        dispatch(addFavorite(id));
-        setShowDropDown(false);
-        setQuery('');
-    };
-
-    const handleFocus = () => query.length >= 2 ? setShowDropDown(true) : null;
 
     return (
         <>
             <AppBar>
                 <SearchInput
-                    placeholder='Search tournaments...'
-                    value={query}
-                    onChange={handleChange}
-                    onFocus={handleFocus}
                     className={classes.searchInput}
-                    error={error}
                     type='text'
                     fullWidth
                     autoFocus
+                    {...props}
                 />
             </AppBar>
             <Toolbar className={classes.toolbar}/>
-            {
-                showDropDown ? <SearchDropdown
-                    listItems={foundTournaments}
-                    isLoading={isLoading}
-                    addFavorite={addFavoriteAction}/> : null
-            }
         </>
     )
 }
